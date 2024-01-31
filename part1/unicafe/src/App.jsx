bf4ff72796b1940fadd8ea022542a8fb31311125
nico <nico@spinvilanova.org>
2024-01-31 18:44:36 +0100

    p 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,35 @@
 import { useState } from "react";
 
+const Statistics = (props) => {
+  const getAll = () => {
+    return props.good + props.neutral + props.bad;
+  };
+
+  const getAverage = () => {
+    if (getAll() === 0) return "No feedback given";
+    return props.total / getAll();
+  };
+
+  const getPositive = () => {
+    if (getAll() === 0) return "No feedback given";
+    return (props.good / getAll()) * 100;
+  };
+
+
+
+  return (
+    <div>
+      <h2>statistics</h2>
+      <p>good {props.good}</p>
+      <p>neutral {props.neutral}</p>
+      <p>bad {props.bad}</p>
+      <p>all {getAll()}</p>
+      <p>average {getAverage()}</p>
+      <p>positive {getPositive()}%</p>
+    </div>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
@@ -11,20 +41,6 @@ const App = () => {
     setTotal(total + points);
   };
 
-  const getAll = () => {
-    return good + neutral + bad;
-  };
-
-  const getAverage = () => {
-    if (getAll() === 0) return "No feedback given";
-    return total / getAll();
-  };
-
-  const getPositive = () => {
-    if (getAll() === 0) return "No feedback given";
-    return (good / getAll()) * 100;
-  };
-
   return (
     <div>
       <h1>give feedback</h1>
@@ -33,13 +49,7 @@ const App = () => {
         neutral
       </button>
       <button onClick={() => handleClick(bad, setBad, -1)}>bad</button>
-      <h2>statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {getAll()}</p>
-      <p>average {getAverage()}</p>
-      <p>positive {getPositive()}%</p>
+      <Statistics total={total} good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
