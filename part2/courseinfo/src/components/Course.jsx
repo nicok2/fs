76ee0fca5d2763386b44883a7ad8d1dbae65c53f
nico <nico@spinvilanova.org>
2024-02-21 21:23:33 +0100

    p 2.1

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,15 @@
+import Header from "../Header";
+import Content from "./Content";
+import Total from "./Total";
+
+const Course = ({ course }) => {
+  const { name, parts } = course;
+  return (
+    <>
+      <Header course={name} />
+      <Content parts={parts} />
+    </>
+  );
+};
+
+export default Course;
